23df11d5b7a62af3d9b7435ba83629358b361cbe
Floaterest <floaterest@outlook.jp>
2020-12-13 20:31:54 -0500

    add js files

---
+++ tab.js
@@ -0,0 +1,23 @@
+// version 20210320
+// show/hide selection menu
+content.addEventListener('click', () => {
+	if(content.style.filter){
+	    content.style.filter = select.style.display = '';
+	}else{
+		content.style.filter = 'brightness(10%)';
+		select.style.display = 'inline';
+	}
+});
+
+// show/hide chapters
+Array.from(select.getElementsByTagName('p'))
+    .forEach(p => p.addEventListener('click', () => {
+	p.classList.toggle('off');
+	document.getElementById(p.innerText)
+	    .style.display = p.classList.contains('off') ? 'none' : 'block';
+    }));
+
+// click all the selections
+document.getElementById('inverse').addEventListener('click', () =>
+    Array.from(select.getElementsByTagName('p')).forEach(p => p.click())
+);
