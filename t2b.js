cd34f8c667266aafdebc301a811078d3e8fa12d9
floaterest <56704092+floaterest@users.noreply.github.com>
2020-12-22 16:55:34 -0500

    remove t2b.js

--- t2b.js
+++
@@ -1,6 +0,0 @@
-// version 20210320
-Array.from(document.getElementsByTagName('img')).forEach(
-    i => i.addEventListener('click',
-        () => i.src = i.getAttribute('src') ? '' : i.alt
-    )
-);
