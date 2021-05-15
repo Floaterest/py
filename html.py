2362a05235f8b15acde3feb50e8cd149202284d0
Floaterest <56704092+Floaterest@users.noreply.github.com>
2021-05-15 16:37:59 -0400

    style(html): better tostr

--- html.py
+++ html.py
@@ -2,9 +2,11 @@ from __future__ import annotations
 import os
 import json
 from codecs import open
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 EXT = ['.png', '.jpg', '.gif']
+VOID = ['meta', 'img']
+NOINDENT = ['head', 'body']
 RED = '\033[1;31m'
 RESET = '\033[0;0m'
 path = os.path
@@ -12,30 +14,41 @@ path = os.path
 
 @dataclass
 class E:
-    void: bool
     tag: str
-    attr: dict
-    text: list[str]
-    children: list[E]
+    attr: dict = field(default_factory=dict)
+    text: str = field(default_factory=str)
+    children: list[E] = field(default_factory=list)
 
-    def tolist(self):
+    def str(self) -> str:
         # start tag
-        s = f'<{self.tag}'
+        s = '<' + self.tag
         # add attributes
         for attr, value in self.attr.items():
             s += f' {attr}="{value}"'
-        if self.void:
-            return [s + '/>']
-        elif not len(self.children) and len(self.text) < 2:
+        if self.tag in VOID:
+            return s + '/>\n'
+        elif not len(self.children) and '\n' not in self.text:
             # if no children and text is at most 1 line
-            return [s + '>' + ''.join(self.text) + '</' + self.tag + '>']
+            return s + '>' + self.text + '</' + self.tag + '>\n'
         else:
-            l = [s + '>']
-            # text
-            for t in self.text:
-                l.append('\t' + t)
-
+            s += '>\n'
+            if self.text:
+                s += '\t' + self.text + '\n'
             # children
             for ch in self.children:
-                l.append('\t' + ch.tolist())
-            return l + [f'</{self.tag}>']
+                if ch.tag not in NOINDENT:
+                    s += '\t'
+                s += ch.str()
+            return s + '</' + self.tag + '>\n'
+
+
+e = E('html', attr={'lang': 'en'}, children=[
+    E('head', attr={'content': 'text/html;charset=utf-8'}, children=[
+        E('title', text='py')
+    ]),
+
+])
+
+with open('t.html', 'w', 'utf8') as f:
+    f.write('<!doctype html>\n')
+    f.write(e.str())
