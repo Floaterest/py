fab2620edd1860070eaaf220661e8e478562cfd9
floaterest <56704092+floaterest@users.noreply.github.com>
2021-05-15 16:25:33 -0400

    feat(html): add DOM element model

--- html.py
+++ html.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import os
 import json
 from codecs import open
@@ -9,3 +10,32 @@ RESET = '\033[0;0m'
 path = os.path
 
 
+@dataclass
+class E:
+    void: bool
+    tag: str
+    attr: dict
+    text: list[str]
+    children: list[E]
+
+    def tolist(self):
+        # start tag
+        s = f'<{self.tag}'
+        # add attributes
+        for attr, value in self.attr.items():
+            s += f' {attr}="{value}"'
+        if self.void:
+            return [s + '/>']
+        elif not len(self.children) and len(self.text) < 2:
+            # if no children and text is at most 1 line
+            return [s + '>' + ''.join(self.text) + '</' + self.tag + '>']
+        else:
+            l = [s + '>']
+            # text
+            for t in self.text:
+                l.append('\t' + t)
+
+            # children
+            for ch in self.children:
+                l.append('\t' + ch.tolist())
+            return l + [f'</{self.tag}>']
