9141f80e94d86b059f9b072e2e5d0043a8fffd1b
floaterest <floaterest@users.noreply.github.com>
2023-10-01 16:30:07 -0400

    feat(renm) works

--- src/renm.rs
+++ src/renm.rs
@@ -1,54 +1,50 @@
 use crate::tree::tree;
 use itertools::Itertools;
-use std::fs::{read_dir, read_to_string, rename, write};
-use std::fs::{remove_dir, DirBuilder};
+use std::fs::DirBuilder;
 use std::io::Result;
 use std::path::{Path, PathBuf};
+use std::{fs, iter};
 
 const PATH: &str = "/tmp/tree.txt";
 
-pub fn run(path: &PathBuf) -> Result<()> {
-    let bs = tree(path, &mut |_, files| Ok(files.to_vec()))?;
-    let bs: Vec<_> = bs.iter().flatten().flat_map(|p| p.strip_prefix(path).ok()).sorted().collect();
-    write(PATH, bs.iter().flat_map(|b| b.to_str()).join("\n"))?;
-    println!("Press enter to continue: ");
+fn input(path: &PathBuf) -> Result<Vec<PathBuf>> {
     let mut line = String::new();
-    std::io::stdin().read_line(&mut line)?;
-    let mut dest = read_to_string(PATH)?;
-    let mut cs: Vec<_> = dest.split("\n").collect();
     loop {
-        let mut dup = cs.iter().duplicates();
+        std::io::stdin().read_line(&mut line)?;
+        let content = fs::read_to_string(path)?;
+        let content: Vec<_> = content.split("\n").map(PathBuf::from).collect();
+        let mut dup = content.iter().duplicates();
         if let Some(d) = dup.next() {
-            println!("{PATH} contains duplicates!");
-            println!("{d}");
-            for line in dup {
-                println!("{line}");
+            println!("{} contains duplicates! Press enter to continue: ", path.to_str().unwrap());
+            for line in iter::once(d).chain(dup) {
+                println!("{}", line.to_str().unwrap());
             }
-            std::io::stdin().read_line(&mut line)?;
-            dest = read_to_string(PATH)?;
-            cs = dest.split("\n").collect();
-            continue;
+        } else {
+            return Ok(content);
         }
-        break;
     }
-    for (bp, c) in bs.iter().zip(cs.iter()) {
-        let b = bp.to_str().unwrap();
-        if &b == c {
-            println!("Skip {b}");
+}
+
+pub fn run(path: &PathBuf) -> Result<()> {
+    let xs = tree(path, &mut |_, files| Ok(files.to_vec()))?;
+    let xs: Vec<_> = xs.into_iter().flatten().sorted().collect();
+    let t = xs.iter().flat_map(|p| p.strip_prefix(path).ok()).flat_map(|p| p.to_str()).join("\n");
+    fs::write(PATH, t)?;
+    println!("Tree written to {PATH}, press enter to continue: ");
+    let ys = input(&PathBuf::from(PATH))?;
+    let ys: Vec<_> = ys.iter().map(|p| Path::join(path, p)).collect();
+    for (x, y) in iter::zip(xs.iter(), ys.iter()) {
+        let (sx, sy) = (x.strip_prefix(path).unwrap(), y.strip_prefix(path).unwrap());
+        let (sx, sy) = (sx.to_str().unwrap(), sy.to_str().unwrap());
+        if x == y {
+            println!("Skip {sx}");
             continue;
         }
-        let (bc, cc) = (Path::join(path, b), Path::join(path, c));
-        if let Some(parent) = cc.parent() {
+        if let Some(parent) = y.parent() {
             DirBuilder::new().recursive(true).create(parent)?;
         }
-        rename(bc, cc)?;
-        println!("{b} -> {c}");
-    }
-    for d in bs.iter().map(|b| b.parent()).dedup() {
-        if read_dir(d)?.next().is_none() {
-            remove_dir(d)?;
-            println!("Remove {}", d.to_str().unwrap());
-        }
+        fs::rename(x, y)?;
+        println!("{sx} -> {sy}");
     }
     Ok(())
 }
